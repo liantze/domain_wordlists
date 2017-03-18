8d8a77e545abffa88efcaa02294ad50facc77447
liantze <liantze@gmail.com>
2017-03-18 15:44:58 +0800

    Checks against hunspell en_US

--- do_linguistics.py
+++ do_linguistics.py
@@ -6,7 +6,7 @@ import hunspell
 
 hobj = hunspell.HunSpell('en_US.dic', 'en_US.aff')
 
-neurowords = set()
+domain_terms = set()
 # english_vocab = set(w.lower() for w in nltk.corpus.words.words())
 
 def extract_tags(url, tag):
@@ -14,23 +14,21 @@ def extract_tags(url, tag):
     terms=[nltk.word_tokenize(t.text) for t in soup(tag)]
     merged = list(itertools.chain(*terms))
     return set([ (w.lower() if w.istitle() else w) for w in merged if w.isalpha() and len(w) > 2 and not(w.isupper()) and not(hobj.spell(w) or hobj.spell(w.lower())) ])
-#    return set([ w for w in merged if w.isalpha() and len(w) > 2 and not(w.isupper()) and not(hobj.spell(w) or hobj.spell(w.lower())) ])
 
 # Erm, copyright? erm?
-url = u'https://www.ncbi.nlm.nih.gov/books/NBK10981/'
+# not much "out of English vocab" words
+url = u'http://www.cs.bham.ac.uk/~pxc/nlp/nlpgloss.html'
 # We want the <dt></dt> bits
-neurowords.update(extract_tags(url, u'dt'))
+domain_terms.update(extract_tags(url, u'strong'))
 
 # ditto re the copyright
-url = 'http://michaeldmann.net/glossary.html'
-# For this one we want the <strong></strong> bits and the first character
-# needs to be lowercased
-neurowords.update(extract_tags(url, u'strong'))
+url = 'https://www.uni-due.de/ELE/LinguisticGlossary.html'
+domain_terms.update(extract_tags(url, u'b'))
 
 # Remove common English words—at least, according to nltk
 # (Might be better to compare against hunspell instead but I dunno how)
-# neurowords = neurowords - english_vocab
+# domain_terms = domain_terms - english_vocab
 
-with open('neuroscience-en.txt', 'w') as file_handler:
-    for item in sorted(neurowords):
+with open('linguistics-en.txt', 'w') as file_handler:
+    for item in sorted(domain_terms):
         status = file_handler.write("{}\n".format(item))
--- do_maths.py
+++ do_maths.py
@@ -2,15 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 import nltk
 import itertools
+import hunspell
+
+hobj = hunspell.HunSpell('en_US.dic', 'en_US.aff')
 
 mathwords = set()
-english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+# english_vocab = set(w.lower() for w in nltk.corpus.words.words())
 
 def extract_tags(url, tag):
     soup = BeautifulSoup(requests.get(url).text, "html.parser")
     terms=[nltk.word_tokenize(t.text) for t in soup(tag)]
     merged = list(itertools.chain(*terms))
-    return set([ w for w in merged if w.isalpha() and len(w) > 2 and not(w.isupper())])
+    return set([ w for w in merged if w.isalpha() and len(w) > 2 and not(w.isupper()) and not(hobj.spell(w) or hobj.spell(w.lower())) ])
 
 ### Mmmm these are nicely formatted as dt-dd
 urls = [u'https://en.wikipedia.org/wiki/Glossary_of_algebraic_geometry',
@@ -37,7 +40,7 @@ for url in urls:
 
 # Remove common English words—at least, according to nltk
 # (Might be better to compare against hunspell instead but I dunno how)
-mathwords = mathwords - english_vocab
+# mathwords = mathwords - english_vocab
 
 with open('maths-en.txt', 'w') as file_handler:
     for item in sorted(mathwords):
