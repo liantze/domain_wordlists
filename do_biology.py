8ba9c25d964044a448cfd90f2e5ecdb0cf73d14b
liantze <liantze@gmail.com>
2017-04-02 23:37:29 +0800

    Extract from more Wikipedia pages for Biology

--- do_biology.py
+++ do_biology.py
@@ -6,7 +6,7 @@ import hunspell
 
 hobj = hunspell.HunSpell('en_US.dic', 'en_US.aff')
 
-mathwords = set()
+biowords = set()
 # english_vocab = set(w.lower() for w in nltk.corpus.words.words())
 
 def extract_tags(url, tag):
@@ -15,16 +15,49 @@ def extract_tags(url, tag):
     merged = list(itertools.chain(*terms))
     return set([ (w.lower() if w.istitle() else w) for w in merged if w.isalpha() and len(w) > 2 and not(w.isupper()) and not(hobj.spell(w) or hobj.spell(w.lower())) ])
 
-urls = [u'https://en.wikipedia.org/wiki/Glossary_of_biology'
+# With <dt>
+urls = [u'https://en.wikipedia.org/wiki/Glossary_of_ant_terms',
+        u'https://en.wikipedia.org/wiki/Glossary_of_bird_terms',
+        u'https://en.wikipedia.org/wiki/Glossary_of_phytopathology',
+        u'https://en.wikipedia.org/wiki/Glossary_of_viticulture_terms',
+        u'https://en.wikipedia.org/wiki/Glossary_of_entomology_terms',
+        u'https://en.wikipedia.org/wiki/Glossary_of_invasion_biology_terms',
+        u'https://en.wikipedia.org/wiki/Glossary_of_neuroanatomy',
         ]
 
 for url in urls:
-    mathwords.update(extract_tags(url, u'b'))
+    biowords.update(extract_tags(url, u'dt'))
+
+
+# With <b>
+urls = [u'https://en.wikipedia.org/wiki/Glossary_of_biology',
+        u'https://en.wikipedia.org/wiki/Glossary_of_arachnology_terms',
+        u'https://en.wikipedia.org/wiki/Glossary_of_gene_expression_terms',
+        ]
+
+for url in urls:
+    biowords.update(extract_tags(url, u'b'))
+
+# With <h2>
+urls = [u'https://en.wikipedia.org/wiki/Glossary_of_Asteraceae-related_terms',
+        ]
+
+for url in urls:
+    biowords.update(extract_tags(url, u'h2'))
+
+# With <td> siiigh
+urls = [u'https://en.wikipedia.org/wiki/Glossary_of_leaf_morphology',
+        u'https://en.wikipedia.org/wiki/Glossary_of_mammalian_dental_topography',
+        ]
+
+for url in urls:
+    biowords.update(extract_tags(url, u'td'))
+
 
 # Remove common English words—at least, according to nltk
 # (Might be better to compare against hunspell instead but I dunno how)
-# mathwords = mathwords - english_vocab
+# biowords = biowords - english_vocab
 
 with open('biology-en.txt', 'w') as file_handler:
-    for item in sorted(mathwords):
+    for item in sorted(biowords):
         status = file_handler.write("{}\n".format(item))
